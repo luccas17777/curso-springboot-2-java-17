2b6d33c4349f497ae314fa91208ab57aac1aa3e1
Luccas Eduardo <luccasalzira@gmail.com>
2022-09-12 16:27:53 -0300

    Service Layer, component registration

--- src/main/java/com/educandoweb/course/resource/UserResource.java
+++ src/main/java/com/educandoweb/course/resource/UserResource.java
@@ -1,19 +1,31 @@
 package com.educandoweb.course.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	@Autowired
+	private UserService service;
 	
-	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria","Maria@gmail.com","9999999","123");
-		return ResponseEntity.ok().body(u);
+	@GetMapping // Serve para mapear uma requisição do tipo Get
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){ //Para o spring aceitar o id e consideralo como parametro que vai checar pela Url é necessario esse @PathVariable
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
